61ce6b1e12a5fe9911340143bca7088ec865df70
Kevin Pandu <162404229+Infoonlygit@users.noreply.github.com>
2024-03-17 22:24:18 +0700

    Create Unboxing.sh

---
+++ Unboxing.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+
+echo "Selamat datang dia bio data saya"
+echo "1. siapa nama saya"
+echo "2. berapa usia saya"
+echo "3. dimana alamat saya"
+echo "4. berapa nomor whatsapp saya"
+
+read - "pilih (1/2/3/4/5):" pil
+if [ $pil = "1"]
+then kevin pandu samudera
+
+if [ $pil = "2"]
+then 14 years old
+
+if [ $pil = "3"]
+then batam kepulauan riau
+
+if [ $pil = "4"]
+then 6281535298570
+else
+ echo "input salah!"
+ fi
