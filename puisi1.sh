18f80927f55839624c1fdff53ceb61c8de495f26
Kevin Pandu <162404229+Infoonlygit@users.noreply.github.com>
2024-04-21 13:57:07 +0700

    Update and rename TexAsciiKevin.sh to puisi1.sh

---
+++ puisi1.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+echo "Ketika pagi aku datang
+Senyummu selalu terkembang
+Menyapaku di pintu gerbang
+
+Kau tepuk pundakku dengan tenang
+Memberiku semangat untuk menang
+Melawan rasa tak senang
+
+Guruku yang kusayang
+Engkau laksana bintang
+Hapuskan gelap dalam terang
+
+Teruslah menjadi tempatku berpegang
+Tempatku bersandar di kala tegang
+Memberiku bekal di masa datang"
